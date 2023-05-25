068686d56e2bdd1f0b80a5650902a3d8748acd45
stecy1 <stecy@solutech.co.ke>
2023-05-25 07:33:46 +0300

    commit

--- scratch.py
+++ scratch.py
@@ -1,3 +1,8 @@
 numbers = [1, 2, 3, 4, 5]
 numbers.append(6)
 print(numbers)
+names = ["Aminah", "Aisha", "Cynthia", "Mwende",]
+#print first index as a mispled value
+names[0] = "Amina"
+print(names[0:3])
+print(names)
